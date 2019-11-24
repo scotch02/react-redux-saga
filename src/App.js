ca9743f352f2760be8835b6b352e86c864943688
Serhii Utiupin <sergey.utupin@gmail.com>
2019-11-25 00:25:17 +0200

    added material-ui Pricing template as prototype

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import RRSAppBar from "./components/RRSAppBar/RRSAppBar";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
+import { createMuiTheme } from "@material-ui/core";
+import { purple, green } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green,
+  },
+  status: {
+    danger: 'orange',
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RRSAppBar />
+      </ThemeProvider>
+    </>
   );
 }
 
--- src/components/RRSAppBar/RRSAppBar.js
+++ src/components/RRSAppBar/RRSAppBar.js
@@ -1,8 +1,108 @@
-import React from 'react'
+import AppBar from "@material-ui/core/AppBar";
+import React from "react";
+import { Toolbar, Typography, Link, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
-function RRSAppBar(props) {
-    return (
-        
-        <div></div>
-    )
+
+
+const useStyles = makeStyles(theme => ({
+  "@global": {
+    body: {
+      backgroundColor: theme.palette.common.white
+    },
+    ul: {
+      margin: 0,
+      padding: 0
+    },
+    li: {
+      listStyle: "none"
+    }
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`
+  },
+  toolbar: {
+    flexWrap: "wrap"
+  },
+  toolbarTitle: {
+    flexGrow: 1
+  },
+  link: {
+    margin: theme.spacing(1, 1.5)
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6)
+  },
+  cardHeader: {
+    backgroundColor: theme.palette.grey[200]
+  },
+  cardPricing: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "baseline",
+    marginBottom: theme.spacing(2)
+  },
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up("sm")]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6)
+    }
+  }
+}));
+
+export default function RRSAppBar(props) {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+      <Toolbar className={classes.toolbar}>
+        <Typography
+          variant="h6"
+          color="inherit"
+          noWrap
+          className={classes.toolbarTitle}
+        >
+          Company name
+        </Typography>
+        <nav>
+          <Link
+            variant="button"
+            color="textPrimary"
+            href="#"
+            className={classes.link}
+          >
+            Features
+          </Link>
+          <Link
+            variant="button"
+            color="textPrimary"
+            href="#"
+            className={classes.link}
+          >
+            Enterprise
+          </Link>
+          <Link
+            variant="button"
+            color="textPrimary"
+            href="#"
+            className={classes.link}
+          >
+            Support
+          </Link>
+        </nav>
+        <Button
+          href="#"
+          color="primary"
+          variant="outlined"
+          className={classes.link}
+        >
+          Login
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
 }
